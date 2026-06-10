2bc2d1665f58591757a640246b4ad4f2a67a2b10
Amina Riaz <maanriaz0325@gmail.com>
2026-06-11 03:24:12 +0500

    fix model

--- api/generate-titles.js
+++ api/generate-titles.js
@@ -67,7 +67,7 @@ RULES:
         "HTTP-Referer": "https://youtube-title-generator.vercel.app",
       },
       body: JSON.stringify({
-        model: "google/gemma-3-27b-it:free",
+        model: "meta-llama/llama-3.3-70b-instruct:free",
         messages: [{ role: "user", content: prompt }]
       })
     });
